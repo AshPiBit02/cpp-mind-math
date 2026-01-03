b04097c2bc7c393785360310c9cef4632d2b15b0
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-03 18:08:30 +0545

    Count valid splits where left sum > right sum of the subarray

---
+++ validsplitcount.cpp
@@ -0,0 +1,24 @@
+//To count how many ways an array can split into two non-empty parts such that the sum of the left part 
+//is strictly greater than the sum of the right part
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={10,4,-8,7,3,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int totalSum=0,prefixSum=0,count=0;
+    for(int i=0;i<n;i++)
+    {
+        totalSum+=arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        prefixSum+=arr[i];
+        if(prefixSum*2>totalSum)
+        {
+            count++;
+        }
+    }
+    cout<<"The number of valid split is "<<count<<endl;
+
+}
