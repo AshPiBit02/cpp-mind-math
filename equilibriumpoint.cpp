b9411a7baa341113adbc3930605b6e3df2b65f33
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-04 22:11:57 +0545

    tiny correction in equilibriumpoint.cpp

--- equilibriumpoint.cpp
+++ equilibriumpoint.cpp
@@ -1,9 +1,10 @@
 //This program follow prefix sum approach to find the equilibrium point(sum of left subarray equal to sum of right subarray) of an array.
+//Note : This approach works only when all elements are positive or negative or zero.
 #include<iostream>
 using namespace std;
 int main()
 {
-    int arr[]={5,1,0,0,8,6};
+    int arr[]={5,3,0,3,2};
     int n=sizeof(arr)/sizeof(arr[0]),totalsum=0;
     for(int i=0;i<n;i++)
     {
