b3714712580f9d009288ad01ac36bad5d815d0ab
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-05 22:40:03 +0545

    Determine Insubsequence

---
+++ inSubsequence.cpp
@@ -0,0 +1,22 @@
+// Given two strings str1 and str2, determine if str2 is a subsequence of str1.
+// A subsequence means all characters of str2 appear in str1 in the same order,
+// but not necessarily contiguously.
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+    string str1 ="ahcdbgcd";
+    string str2 ="abc";
+    int j=0;
+
+    for (int i=0;i<str1.size() && j<str2.size();i++) {
+        if (str1[i]==str2[j]) {
+            j++;
+        }
+    }
+
+    cout<<((j==str2.size())?"Is Subsequence":"Is not Subsequence");
+    return 0;
+}
