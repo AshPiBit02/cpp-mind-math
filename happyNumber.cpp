f7c929fe6e34fa3577d6e97edb1b77698064ba5c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-06 13:15:12 +0545

    happer number

---
+++ happyNumber.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<set>
+using namespace std;
+bool isHappy(int num)
+{
+    set<int>s;
+    while(num!=1)
+    {
+        if(s.count(num))
+        {
+            return false;
+        }
+        s.insert(num);
+        int sum=0;
+        while(num>0)
+        {
+            int rem=num%10;
+            sum+=rem*rem;
+            num/=10;
+        }
+        num=sum;
+    }
+    return true;
+}
+int main()
+{
+    int num=37;
+    if(isHappy(num))
+    {
+        cout<<"True"<<endl;
+    }
+    else
+    {
+        cout<<"False"<<endl;
+    }
+    return 0;
+
+}
