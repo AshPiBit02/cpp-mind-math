ce6ddf809d38e0d22193c6c269751bb7cb8f966a
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-08 21:48:07 +0545

    rotating string by k steps to the right

--- rotatestringbyKsteps.cpp
+++ rotatestringbyKsteps.cpp
@@ -2,17 +2,21 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+ string rotate(string& str,int k)
+{
+    int n=str.length();
+    k=k%n;//handles if k>n
+    reverse(str.begin(),str.end());
+    reverse(str.begin(),str.begin()+k);
+    reverse(str.begin()+k,str.end());
+    return str;
+}
 int main()
 {
     int k=3;
     string str;
     cout<<"Enter a string: ";
     getline(cin,str);
-    reverse(str.begin(),str.end());
-    reverse(str.begin(),str.begin()+k);
-    reverse(str.begin()+k,str.end());
-    cout<<"The string after rotating to right by "<<k<<" steps is : "<<str<<endl;
+    cout<<"The string after rotating to right by "<<k<<" steps is : "<<rotate(str,k)<<endl;
     return 0;
-
-
 }
