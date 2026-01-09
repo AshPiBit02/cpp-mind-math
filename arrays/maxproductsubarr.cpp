bb3a8ecdd6f234a6df2dcaa53ad9cac935e0a8e3
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-09 23:55:19 +0545

    maxproductsubarr fully corrected version

--- arrays/maxproductsubarr.cpp
+++ arrays/maxproductsubarr.cpp
@@ -1,30 +1,44 @@
 //find the maximum product subarray.
 #include<iostream>
+#include<algorithm>
 using namespace std;    
 int main()
 {
     int arr[]={1,-2,-3,0,7,-8,-2};
     int n=sizeof(arr)/sizeof(arr[0]);
     int max_product=arr[0];
-    int current_product=1;
-    int start=0,current_start=0,max_len=1;
+    int min_product=arr[0];
+    int result=arr[0];
+    int start=0,end=0,temp_start=0;
     for(int i=0;i<n;i++)
     {
-        current_product*=arr[i];
-        if(current_product>max_product)
+        //swap when negative
+        if(arr[i]<0)
         {
-            max_product=current_product;
-            start=current_start;
-            max_len=i-current_start+1;
+            swap(max_product,min_product);
         }
-        if(current_product==0)
+        //update max product
+        if(arr[i]>max_product*arr[i])
         {
-            current_product=1;
-            current_start=i+1;
+            max_product=arr[i];
+            temp_start=i;
+        }
+        else
+        {
+            max_product*=arr[i];
+        }
+        //update min product
+        min_product=min(arr[i],min_product*arr[i]);
+        // update result
+        if(max_product>result)
+        {
+            result=max_product;
+            start=temp_start;
+            end=i;
         }
     }
     cout<<"The maximum product subarray: ";
-    for(int i=start;i<start+max_len;i++)
+    for(int i=start;i<=end;i++)
     {
         cout<<arr[i]<<" ";
     }
