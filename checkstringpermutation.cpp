8d5c2a3af4ff71888ce413e3beedbd6cd3b15b4c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-14 11:19:11 +0545

    Checkstring permutation

---
+++ checkstringpermutation.cpp
@@ -0,0 +1,55 @@
+//check if the str2 contains the permutation of the string present in str1
+#include<iostream>
+#include<string>
+using namespace std;
+bool isFreqSame(int freq[],int* windowfreq)//in function parameter int freq[] & int* freq both are same
+{
+    for(int i=0;i<26;i++)
+    {
+        if(freq[i]!=windowfreq[i])
+        {
+            return false;
+        }
+    }
+    return true;
+
+}
+bool checkInclusion(string str1,string str2)
+{
+    int freq[26]={0};// stores frequencies of all characters
+    for(int i=0;i<str1.length();i++)
+    {
+        freq[str1[i]-'a']++;//idx=str1[i]-'a' stores the frequency of str1 in frequency array
+    }
+    int windowSize=str1.length();//the windowsize will always be equal as the size of array whose permutation to be check. clearly
+    for(int i=0;i<str2.length();i++)
+    {
+        int windowIdx=0,idx=i;
+        int windFreq[26]={0};//windowFreq stores all frequency of the characters in current window
+        while(windowIdx<windowSize && idx<str2.length())
+        {
+            windFreq[str2[idx]-'a']++;
+            windowIdx++;
+            idx++;
+        }
+        if(isFreqSame(freq,windFreq))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    string str1="aoe";
+    string str2="edibaooo";
+    if(checkInclusion(str1,str2))
+    {
+        cout<<"Permutation found..."<<endl;
+    }
+    else
+    {
+        cout<<"Permutation not found!"<<endl;
+    }
+    return 0;
+}
