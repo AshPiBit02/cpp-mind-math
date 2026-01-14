c114a89aad46781ce4fca73db6d1fc3e5272b727
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-14 12:29:26 +0545

    slight improvement

--- stringcompress.cpp
+++ stringcompress.cpp
@@ -8,7 +8,7 @@ int compress(vector<char>&chars)
 {
     int n=chars.size();
     int idx=0;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;)
     {
         char ch=chars[i];
         int count=0;
@@ -29,14 +29,25 @@ int compress(vector<char>&chars)
                 chars[idx++]=dig;
             }
         }
-        i--;
     }
     chars.resize(idx);
     return idx;
 }
 int main()
 {
-    vector<char>chars={'a','a','b','b','b','c','c','c'};
-    cout<<compress(chars);
+    vector<char>chars={'a','a','b','b','b','c','c','c'};//a 2 b 3 c 2
+    cout<<"Before compression: ";
+    for(char i: chars)
+    {
+        cout<<i<<" ";
+    }
+    cout<<"|  size: "<<chars.size();
+    compress(chars);
+    cout<<"\nAfter compression: ";
+    for(char i:chars)
+    {
+        cout<<i<<" ";
+    }
+    cout<<"|  size: "<<compress(chars);
     return 0;
 }
