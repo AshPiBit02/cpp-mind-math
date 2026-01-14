6d1f7da1fd03dbcabb2d31d0e9686a457a64d9c7
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-14 16:36:16 +0545

    better

--- GCD.cpp
+++ GCD.cpp
@@ -3,33 +3,21 @@
 using namespace std;
 int gcd(int x,int y)
 {
-    if(x==0 || y==0)
+    while(x>0 && y>0)
     {
-        if(x==0)
+        if(x>y)
         {
-            return y;
+            x%=y;
         }
         else
         {
-            return x;
-        }
-    }else{
-
-        if(x>y)
-        {
-            return gcd(x%y,y);//mode will continuously subtract if x is far greater than y so 
-            //we don't need to subtract iterately as we get exact value after multiple substraction and single mod use
-
-        }
-        else //if y>=x 
-        {
-            return gcd(x,y%x);
+            y%=x;
         }
     }
+    return x==0?y:x;
     }
 int main()
 {
-    int x=66,y=100;
-    cout<<gcd(x,y);
+    cout<<gcd(16,20)<<endl;
     return 0;
 }
