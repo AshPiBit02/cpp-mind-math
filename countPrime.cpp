55fc9c1566c1fbc516c761008440bd45dffe512c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-14 14:00:48 +0545

    count prime by Sieve of Eratosthenes

---
+++ countPrime.cpp
@@ -0,0 +1,27 @@
+//Count number of prime numbers between given range
+#include<iostream>
+#include<vector>
+using namespace std;
+int countPrime(int n)
+{
+    vector<bool>isPrime(n+1,true);
+    int count=0;
+    for(int i=2;i<n;i++)
+    {
+        if(isPrime[i])
+        {
+            count++;
+            for(int j=i*2;j<n;j=j+i)
+            {
+                isPrime[j]=false;
+            }
+        }
+    }
+    return count;
+}
+int main()
+{
+    int n=26;
+    cout<<countPrime(n)<<endl;
+    return 0;
+}
