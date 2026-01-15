712bb98a1144980b082f9a076a272a607d68c128
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-15 15:46:48 +0545

    count pair with absolute difference k

---
+++ arrays/countpairwithabdeffk.cpp
@@ -0,0 +1,25 @@
+//Count Pairs with absolute difference k
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+int count_pair_diff_k(vector<int>&arr,int k)
+{
+    int n=arr.size(),count=0;
+    unordered_map<int,int>mp;
+    for(auto &a: arr)
+    {
+        if(mp.find(a-k)!=mp.end() || mp.find(a+k)!=mp.end())
+        {
+            count++;
+        }
+        mp[a];
+    }
+    return count;
+}
+int main()
+{
+    vector<int>arr={1,5,3,4,2};
+    cout<<count_pair_diff_k(arr,2);
+    return 0;
+}
