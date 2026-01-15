87dd63ee5c4518920de95d9c1dda1d63aa5f12c5
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-15 17:11:49 +0545

    finding duplicate using fast and slow pointer

---
+++ arrays/duplicatefastslow.cpp
@@ -0,0 +1,26 @@
+//Find duplicate element in the array using fast and slow pointer
+#include<iostream>
+#include<vector>
+using namespace std;
+int finduplicate(const vector<int>&arr)
+{
+    int slow=arr[0],fast=arr[0];
+    do
+    {
+        slow=arr[slow];//+1
+        fast=arr[arr[fast]];//+2
+    } while (slow!=fast);
+    slow=arr[0];
+    while(slow!=fast)
+    {
+        slow=arr[slow];//+1
+        fast=arr[fast];//+1
+    }
+    return slow;
+}
+int main()
+{
+    vector<int>arr={2,4,3,1,5,4};
+    cout<<"Duplicate : "<<finduplicate(arr);
+    return 0;
+}
