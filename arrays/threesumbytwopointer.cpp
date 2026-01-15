4128fc609b7007fd5b66180f751adcaf7c7e2c19
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-15 22:05:39 +0545

    Optimized: Three sum using two pointers approach

--- arrays/threesumbytwopointer.cpp
+++ arrays/threesumbytwopointer.cpp
@@ -12,7 +12,7 @@ vector<vector<int>> threesum(vector<int>&arr)
     {
         if(i>0 && arr[i]==arr[i-1]) continue;//handles redundency. skip the part in which the current i index element is equal to the previous one
         int j=i+1,k=n-1;//j points to the element next to i and k points to the end element;
-        while(j!=k)
+        while(j<k)
         {
             int sum=arr[i]+arr[j]+arr[k];
             if(sum>0)
@@ -23,11 +23,15 @@ vector<vector<int>> threesum(vector<int>&arr)
                 j++;//since j points to the left element after i in the sorted array
             }else
             {
-                ans.emplace_back(arr[i],arr[j],arr[k]);
+                ans.push_back({arr[i],arr[j],arr[k]});
                 j++;k--;
-                while(arr[j]==arr[j-1])//handles if the value at j repeats
+                while(j<k && arr[j]==arr[j-1])//handles if the value at j repeats
                 {
                     j++;
+                }
+                while(j<k && arr[k]==arr[k+1])//handles if the value at k repeats
+                {
+                    k--;
                 }
 
             }
@@ -37,7 +41,7 @@ vector<vector<int>> threesum(vector<int>&arr)
 }
 int main()
 {
-    vector<int>arr={-1,0,1,2,-1,-4};
+    vector<int>arr={-1,0,1,2,-1,-3};
     vector<vector<int>>res=threesum(arr);
     for(int i=0;i<res.size();i++)
     {
