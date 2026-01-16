b1d08f51a38f43d9f842fb8b1328752eb847eb92
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-16 22:50:25 +0545

    correction

--- Recursion&Backtracking/findpermustring.cpp
+++ Recursion&Backtracking/findpermustring.cpp
@@ -1,41 +1,26 @@
 //finds permutation of string
 #include<iostream>
 #include<string>
-#include<vector>
 #include<algorithm>
 using namespace std;
-void getPermutation(vector<string>&str,int idx,vector<vector<string>>&ans)
+void getPermutation(string&str,int idx)
 {
     if(idx==str.size())
     {
-        ans.push_back(str);
+        cout << str << endl;
         return;
     }
     for(int i=idx;i<str.size();i++)
     {
         swap(str[idx],str[i]);
-        getPermutation(str,idx+1,ans);
+        getPermutation(str,idx+1);
         swap(str[idx],str[i]);
     }
 }
-vector<vector<string>> permute(vector<string>&str)
-{
-    vector<vector<string>>ans;
-    getPermutation(str,0,ans);
-    return ans;
-}
 int main()
 {
-    vector<string>str={"a","b","c"};
-    vector<vector<string>>res=permute(str);
+    string str={"abc"};
     cout<<"The permutations are: "<<endl;
-    for(int i=0;i<res.size();i++)
-    {
-        for(int j=0;j<res[i].size();j++)
-        {
-            cout<<res[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    getPermutation(str,0);
     return 0;
 }
