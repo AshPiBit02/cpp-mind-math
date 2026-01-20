bdb50444d6dae8625763c6adc2c0b73855e3d7d4
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-20 13:04:31 +0545

    pop func added

---
+++ Linked List/doublyLL.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *next,*prev;
+    Node(int data)
+    {
+        this->data=data;
+        next=nullptr;
+        prev=nullptr;
+    }
+};
+class DLL
+{
+    public:
+    Node *head,*tail;
+    DLL()
+    {
+        head=tail=nullptr;
+    }
+    bool isempty()
+    {
+        return head==nullptr;
+    }
+    void push_head(int data)
+    {
+        Node* newNode= new Node(data);
+        if(isempty())
+        {
+            head=tail=newNode;
+            tail->next=nullptr;
+            head->prev=nullptr;        
+        }
+        else
+        {
+            head->prev=newNode;
+            newNode->next=head;
+            head=newNode;
+            head->prev=nullptr;
+        }
+        cout<<data<<" pushed to head"<<endl;
+    }
+    void push_tail(int data)
+    {
+        Node* newNode=new Node(data);
+        if(isempty())
+        {
+            head=tail=newNode;
+            tail->next=nullptr;
+            head->prev=nullptr;
+        }
+        else
+        {
+            tail->next=newNode;
+            newNode->prev=tail;
+            tail=newNode;
+            tail->next=nullptr;
+        }
+    }
+    void pop_head()
+    {
+        if(isempty())
+        {
+            cout<<"List is empty!"<<endl;
+            return;
+        }
+        Node* temp=head;
+        if(head==tail)
+        {
+            head=tail=nullptr;
+        }else
+        {
+            head=temp->next;
+            head->prev=nullptr;
+        }
+        cout<<temp->data<<" poped from head"<<endl;
+        delete temp;
+    }
+    void pop_tail()
+    {
+        if(isempty())
+        {
+            cout<<"List is empty!"<<endl;
+            return;
+        }
+        Node* temp=tail;
+        if(head==tail)
+        {
+            tail=head=nullptr;
+        }
+        else
+        {
+            tail=temp->prev;
+            tail->next=nullptr;
+        }
+        cout<<temp->data<<" poped from tail"<<endl;
+        delete temp;
+    }
+
+};
+int main()
+{
+    return 0;
+}
